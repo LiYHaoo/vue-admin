4547b3181c5704f542bb2affa9946a4ce40c71e1
liyuanhao <632426290@qq.com>
2020-07-15 16:24:29 +0800

    代码提交

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from './router/router'
+import store from './store/store'
+//import axios from 'axios'
+
+let start = function () {
+  Vue.config.productionTip = false
+  Vue.use(ElementUI);
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app');
+
+
+  // axios.get('./baseUrl.json').then(res => {
+  //   Vue.config.productionTip = false
+  //   Vue.prototype.BASE_URL = res.data.BASE_URL
+  //   Vue.use(ElementUI);
+  //   new Vue({
+  //     router,
+  //     store,
+  //     render: h => h(App)
+  //   }).$mount('#app');
+  // })
+}
+
+start()
---
+++ src/store/modules/login.js
@@ -0,0 +1,19 @@
+const state = {
+    token: '',
+    username: '',
+    authCode: ''
+}
+const result = {
+    code: 200,
+    message: '操作成功'
+}
+
+const getters = {
+   
+}
+const actions = {
+    
+}
+const mutations = {
+    
+}
